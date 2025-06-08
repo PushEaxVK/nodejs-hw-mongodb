dfb92834555742f7b92ef60f7586d68d9e59bc35
PushEax <pusheax.vk@gmail.com>
2025-06-09 00:30:12 +0300

    move controllers

---
+++ src/controllers/contacts.js
@@ -0,0 +1,34 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res) => {
+  const { contactId } = req.params;
+  let contact;
+
+  try {
+    contact = await getContactById(contactId);
+  } catch {
+    contact = null;
+  }
+
+  if (!contact) {
+    res.status(404).json({
+      message: 'Contact not found',
+    });
+    return;
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
