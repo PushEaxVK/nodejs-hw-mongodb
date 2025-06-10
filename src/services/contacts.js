23c185501c99b4f962907448f2bff462e4addcea
PushEax <pusheax.vk@gmail.com>
2025-06-10 10:03:39 +0300

    add patch

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -14,3 +14,22 @@ export const createContact = async (body) => {
   const contact = await ContactsCollection.create(body);
   return contact;
 };
+
+export const updateContact = async (contactId, body, options = {}) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    body,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
