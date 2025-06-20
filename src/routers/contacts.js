648dd8b2a846955bfff88f345d5a853c442e3472
PushEax <pusheax.vk@gmail.com>
2025-06-20 10:01:10 +0300

    add validation to routes post and patch

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,13 +6,26 @@ import {
   getContactsController,
   patchContactController,
 } from '../controllers/contacts.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
 
 const router = Router();
 
 router.get('/contacts', getContactsController);
 router.get('/contacts/:contactId', getContactByIdController);
-router.post('/contacts', createContactController);
-router.patch('/contacts/:contactId', patchContactController);
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  createContactController,
+);
+router.patch(
+  '/contacts/:contactId',
+  validateBody(updateContactSchema),
+  patchContactController,
+);
 router.delete('/contacts/:contactId', deleteContactByIdController);
 
 export default router;
