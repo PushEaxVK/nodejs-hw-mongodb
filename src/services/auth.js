6e5a2fc6022c03be6464a89b8f4a7fbc814ea8ee
PushEax <pusheax.vk@gmail.com>
2025-06-29 01:21:02 +0300

    check for existing email

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,13 @@
+import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/models/users.js';
 
 export const registerUser = async (payload) => {
+  const existingUser = await UsersCollection.findOne({ email: payload.email });
+
+  if (existingUser) {
+    throw createHttpError(409, 'Email in use');
+  }
+
   const user = await UsersCollection.create(payload);
   return user;
 };
