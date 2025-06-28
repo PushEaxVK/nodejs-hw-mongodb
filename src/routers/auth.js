10dbbacabfd79adc7930e2d6bccb78ffc02fd187
PushEax <pusheax.vk@gmail.com>
2025-06-29 00:29:36 +0300

    use validation for register route

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import { validateBody } from '../middlewares/validateBody.js';
+import { registerUserSchema } from '../validation/auth.js';
 
 const router = Router();
 
-router.post('/auth/register', (req, res) => res.json({ message: 'OK' }));
+router.post('/auth/register', validateBody(registerUserSchema), (req, res) =>
+  res.json({ message: 'OK' }),
+);
 router.post('/auth/login', (req, res) => res.json({ message: 'OK' }));
 router.post('/auth/refresh', (req, res) => res.json({ message: 'OK' }));
 router.post('/auth/logout', (req, res) => res.json({ message: 'OK' }));
