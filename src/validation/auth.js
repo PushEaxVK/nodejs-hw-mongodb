9dd27b9eeca4a76369e17bc0077397b00931b622
PushEax <pusheax.vk@gmail.com>
2025-06-29 01:04:19 +0300

    update password validation

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -3,5 +3,12 @@ import Joi from 'joi';
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string()
+    .min(12)
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{12,}$/)
+    .message({
+      'string.pattern.base':
+        'Password must include at least one uppercase letter, one lowercase letter, one digit, and one special character.',
+    })
+    .required(),
 });
