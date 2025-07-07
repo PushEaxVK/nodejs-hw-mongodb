2480fe2d1dc1a7f4226cb80edd418a5031603cc1
PushEax <pusheax.vk@gmail.com>
2025-07-08 01:51:21 +0300

    add upload dir as static route

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { ENV_VARS } from './constants/envVars.js';
+import { UPLOAD_DIR } from './constants/paths.js';
 
 const PORT = Number(getEnvVar(ENV_VARS.PORT, '3000'));
 
@@ -39,6 +40,8 @@ export const setupServer = () => {
   app.use(router);
   app.use(notFoundHandler);
   app.use(errorHandler);
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
